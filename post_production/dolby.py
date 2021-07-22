d2900e0e95b8ffa353b048a2c892bd1ea9a47281
Sean Tibor <40579452+seantibor@users.noreply.github.com>
2021-07-22 09:52:31 -0400

    Refactor Dolby client into class with better API key handling
    - DolbyIO is now a client class
    - API key can be provided from the CLI client
    - API key will be added to environment variables in the CLI example
    - DolbyIO now uses a requests session

--- post_production/dolby.py
+++ post_production/dolby.py
@@ -10,8 +10,6 @@ import requests
 
 logger = logging.getLogger(__name__)
 
-API_KEY = os.environ["DOLBY_API_KEY"]
-
 
 class JobType(Enum):
     ENHANCE = "enhance"
@@ -22,177 +20,193 @@ class JobType(Enum):
     DOWNLOAD = "output"
 
 
-def endpoint(job_type: JobType) -> str:
-    return f"https://api.dolby.com/media/{job_type.value}"
+class DolbyIO:
+    def __init__(self, api_key: str):
+        headers = {
+            "x-api-key": api_key,
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
+        self._session = requests.Session()
+        self._session.headers.update(headers)
+        self._api_key = api_key
 
+    @staticmethod
+    def endpoint(job_type: JobType) -> str:
+        return f"https://api.dolby.com/media/{job_type.value}"
 
-def upload(file_path: Path) -> str:
-    """For a given file, upload it and return a Dolby-compatible url
+    @property
+    def api_key(self):
+        return self._api_key
 
-    Args:
-        in_file (Path): path to local file
+    @api_key.setter
+    def api_key(self, key):
+        self._api_key = key
+        self._session.headers["x-api-key"] = key
 
-    Returns:
-        str: string to remote path
-    """
-    url = endpoint(JobType.UPLOAD)
-    headers = {
-        "x-api-key": API_KEY,
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-    }
+    def upload(self, file_path: Path) -> str:
+        """For a given file, upload it and return a Dolby-compatible url
 
-    in_url = f"dlb://in/{quote(file_path.name)}"
+        Args:
+            in_file (Path): path to local file
 
-    body = {
-        "url": in_url,
-    }
+        Returns:
+            str: string to remote path
+        """
+        url = self.endpoint(JobType.UPLOAD)
 
-    r = requests.post(url, json=body, headers=headers)
-    r.raise_for_status()
-    logger.info(f"Created endpoint for {in_url}")
-    data = r.json()
-    presigned_url = data["url"]
-    # Upload your media to the pre-signed url response
+        in_url = f"dlb://in/{quote(file_path.name)}"
 
-    with open(file_path, "rb") as input_file:
-        requests.put(presigned_url, data=input_file)
+        body = {
+            "url": in_url,
+        }
 
-    logger.info(f"Uploaded {file_path.name} to {in_url}")
-    return in_url
+        r = self._session.post(url, json=body)
+        r.raise_for_status()
+        logger.info(f"Created endpoint for {in_url}")
 
+        data = r.json()
+        presigned_url = data["url"]
+        # Upload your media to the pre-signed url response
 
-def enhance(in_url: str, out_url: Optional[str] = None) -> Tuple[str, str]:
-    """Process an uploaded url and return a file once completed
+        with open(file_path, "rb") as input_file:
+            self._session.put(presigned_url, data=input_file)
 
-    Args:
-        in_url (str): path to Dolby-compatible file
+        logger.info(f"Uploaded {file_path.name} to {in_url}")
+        return in_url
 
-    Returns:
-        str: path to processed file
-    """
-    out_url = in_url.replace("dlb://in/", "dlb://out/")
+    def enhance(self, in_url: str, out_url: Optional[str] = None) -> Tuple[str, str]:
+        """Process an uploaded url and return a file once completed
 
-    body = {
-        "content": {"type": "interview"},
-        "audio": {
-            "loudness": {"enable": True, "dialog_intelligence": True},
-            "dynamics": {"range_control": {"enable": True}},
-            "noise": {"reduction": {"enable": True}},
-            "filter": {
-                "dynamic_eq": {"enable": True},
-                "high_pass": {"enable": True},
+        Args:
+            in_url (str): path to Dolby-compatible file. Can be a Dolby Input file or other S3-compatible storage links
+
+        Returns:
+            str: path to processed file
+        """
+        out_url = in_url.replace("dlb://in/", "dlb://out/")
+
+        body = {
+            "content": {"type": "interview"},
+            "audio": {
+                "loudness": {"enable": True, "dialog_intelligence": True},
+                "dynamics": {"range_control": {"enable": True}},
+                "noise": {"reduction": {"enable": True}},
+                "filter": {
+                    "dynamic_eq": {"enable": True},
+                    "high_pass": {"enable": True},
+                },
             },
-        },
-        "input": in_url,
-        "output": out_url,
-    }
+            "input": in_url,
+            "output": out_url,
+        }
 
-    url = endpoint(JobType.ENHANCE)
-    headers = {
-        "x-api-key": API_KEY,
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-    }
+        url = self.endpoint(JobType.ENHANCE)
 
-    logger.info(f"Beginning enhancement for {in_url}")
-    response = requests.post(url, json=body, headers=headers)
-    response.raise_for_status()
-
-    job_id = response.json().get("job_id")
-    logger.info(f"Created enhancement job {job_id} with expected output {out_url}")
-
-    return job_id, out_url
-
-
-def analyze(
-    in_url: str, out_url: Optional[str] = None, speech: bool = False
-) -> Tuple[str, str]:
-    """Analyzes an uploaded url and returns a json file once completed
-
-    Args:
-        in_url (str): path to Dolby-compatible file
-        out_url (str): path to JSON analysis report
-
-    Returns:
-        str: path to json analysis
-    """
-    out_frags = in_url.split(".")[:-1]
-    out_frags.append("json")
-    out_url = ".".join(out_frags).replace("dlb://in/", "dlb://out/")
-
-    body = {
-        "input": in_url,
-        "output": out_url,
-    }
-    if speech:
-        url = endpoint(JobType.SPEECH_ANALYZE)
-        logger.info(f"Beginning speech analysis for {in_url}")
-    else:
-        url = endpoint(JobType.ANALYZE)
-        logger.info(f"Beginning analysis for {in_url}")
-    headers = {
-        "x-api-key": API_KEY,
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-    }
-
-    response = requests.post(url, json=body, headers=headers)
-    response.raise_for_status()
-    job_id = response.json().get("job_id")
-
-    return job_id, out_url
-
-
-def get_status(job_id: str, job_type: JobType = JobType.ENHANCE) -> Tuple[str, int]:
-    url = endpoint(job_type)
-    headers = {
-        "x-api-key": API_KEY,
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-    }
-
-    params = {"job_id": job_id}
-
-    r = requests.get(url, params=params, headers=headers)
-    r.raise_for_status()
-    data = r.json()
-    return data.get("status"), data.get("progress")
-
-
-def download(
-    out_url: str, out_path: Optional[Path] = None, job_type: JobType = JobType.ENHANCE
-):
-    if not out_path:
-        out_path = Path(__file__).parent / "output"
-
-    if job_type == JobType.ANALYZE:
-        file_tag = " - Analyzed"
-    elif job_type == JobType.SPEECH_ANALYZE:
-        file_tag = " - Speech Analyzed"
-    else:
-        file_tag = " - Enhanced"
-
-    filename = unquote(out_url.split("/")[-1])
-    out_file = out_path / (Path(filename).stem + file_tag + Path(filename).suffix)
-    out_path.mkdir(parents=True, exist_ok=True)
-
-    url = endpoint(JobType.DOWNLOAD)
-    headers = {
-        "x-api-key": API_KEY,
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-    }
-
-    args = {
-        "url": out_url,
-    }
-    logger.info(f"Downloading file from {out_url} to {out_file}")
-    with requests.get(url, params=args, headers=headers, stream=True) as response:
+        logger.info(f"Beginning enhancement for {in_url}")
+        response = self._session.post(url, json=body)
         response.raise_for_status()
-        response.raw.decode_content = True
-        logger.info(f"Downloading from {out_url} into {out_file}")
-        with open(out_file, "wb") as output_file:
-            shutil.copyfileobj(response.raw, output_file)
 
-    return out_file
+        job_id = response.json().get("job_id")
+        logger.info(f"Created enhancement job {job_id} with expected output {out_url}")
+
+        return job_id, out_url
+
+    def analyze(
+        self, in_url: str, out_url: Optional[str] = None, speech: bool = False
+    ) -> Tuple[str, str]:
+        """Analyzes an uploaded url and returns a json file once completed
+
+        Args:
+            in_url (str): path to Dolby-compatible file
+            out_url (str): path to JSON analysis report
+
+        Returns:
+            str: path to json analysis
+        """
+        out_frags = in_url.split(".")[:-1]
+        out_frags.append("json")
+        out_url = ".".join(out_frags).replace("dlb://in/", "dlb://out/")
+
+        body = {
+            "input": in_url,
+            "output": out_url,
+        }
+        if speech:
+            url = self.endpoint(JobType.SPEECH_ANALYZE)
+            logger.info(f"Beginning speech analysis for {in_url}")
+        else:
+            url = self.endpoint(JobType.ANALYZE)
+            logger.info(f"Beginning analysis for {in_url}")
+
+        response = self._session.post(url, json=body)
+        response.raise_for_status()
+        job_id = response.json().get("job_id")
+
+        return job_id, out_url
+
+    def get_status(
+        self, job_id: str, job_type: JobType = JobType.ENHANCE
+    ) -> Tuple[str, int]:
+        """Get the current job status on Dolby.io for the given job id.
+
+        Args:
+            job_id (str): A valid job_id from the dolby.io website
+            job_type (JobType, optional): The type of job to monitor. Defaults to JobType.ENHANCE.
+
+        Returns:
+            Tuple[str, int]: A tuple containing the job status and percent complete, e.g. ("Running", 19)
+        """
+        url = self.endpoint(job_type)
+
+        params = {"job_id": job_id}
+
+        r = self._session.get(url, params=params)
+        r.raise_for_status()
+        data = r.json()
+        return data.get("status"), data.get("progress")
+
+    def download(
+        self,
+        out_url: str,
+        out_path: Optional[Path] = None,
+        job_type: JobType = JobType.ENHANCE,
+    ) -> Path:
+        """Download files from the Dolby.io Media endpoints
+
+        Args:
+            out_url (str): The path to the dolby.io media
+            out_path (Optional[Path], optional): The folder to save the file. Defaults to output/ under the current working directory.
+            job_type (JobType, optional): The job type for naming conventions. Defaults to JobType.ENHANCE.
+
+        Returns:
+            [Path]: The pathlib link to the downloaded file
+        """
+        if not out_path:
+            out_path = Path(__file__).parent / "output"
+
+        if job_type == JobType.ANALYZE:
+            file_tag = " - Analyzed"
+        elif job_type == JobType.SPEECH_ANALYZE:
+            file_tag = " - Speech Analyzed"
+        else:
+            file_tag = " - Enhanced"
+
+        filename = unquote(out_url.split("/")[-1])
+        out_file = out_path / (Path(filename).stem + file_tag + Path(filename).suffix)
+        out_path.mkdir(parents=True, exist_ok=True)
+
+        url = self.endpoint(JobType.DOWNLOAD)
+
+        args = {
+            "url": out_url,
+        }
+        logger.info(f"Downloading file from {out_url} to {out_file}")
+        with self._session.get(url, params=args, stream=True) as response:
+            response.raise_for_status()
+            response.raw.decode_content = True
+            logger.info(f"Downloading from {out_url} into {out_file}")
+            with open(out_file, "wb") as output_file:
+                shutil.copyfileobj(response.raw, output_file)
+
+        return out_file
--- post_production/main.py
+++ post_production/main.py
@@ -1,10 +1,11 @@
 import logging
+import os
 import time
 from pathlib import Path
 
 import click
 
-from post_production import dolby
+from post_production.dolby import DolbyIO, JobType
 
 log_dir = Path("logs")
 log_dir.mkdir(parents=True, exist_ok=True)
@@ -20,22 +21,39 @@ def process(infile, output, analyze, analyze_speech):
     logging.basicConfig(
         filename=log_dir / f"job_logs_{infile.name}.log", level=logging.INFO
     )
+
+    # Make sure we have a valid Dolby API key
+    API_KEY = get_api_key()
+    dolby = DolbyIO(API_KEY)
+
     click.echo(f"Uploading {infile}...")
     in_url = dolby.upload(infile)
 
     if analyze:
         click.echo(f"Analyzing {in_url}...")
         job_id, out_url = dolby.analyze(in_url)
-        job_type = dolby.JobType.ANALYZE
+        job_type = JobType.ANALYZE
     elif analyze_speech:
         click.echo(f"Analyzing speech in {in_url}...")
         job_id, out_url = dolby.analyze(in_url, speech=True)
-        job_type = dolby.JobType.SPEECH_ANALYZE
+        job_type = JobType.SPEECH_ANALYZE
     else:
         click.echo(f"Processing {in_url}...")
         job_id, out_url = dolby.enhance(in_url)
-        job_type = dolby.JobType.ENHANCE
+        job_type = JobType.ENHANCE
 
+    display_status(dolby, job_id, job_type)
+
+    if output:
+        out_path = Path(output)
+    else:
+        out_path = infile.parent / "output"
+    click.echo(f"Downloading file from {out_url} to {out_path}")
+    file_path = dolby.download(out_url=out_url, out_path=out_path, job_type=job_type)
+    click.echo(f"File {infile} processed and saved to {file_path}")
+
+
+def display_status(dolby, job_id, job_type):
     status, pct = dolby.get_status(job_id, job_type=job_type)
     last_pct = 0
     with click.progressbar(
@@ -50,10 +68,11 @@ def process(infile, output, analyze, analyze_speech):
                 break
             time.sleep(1)
 
-    if output:
-        out_path = Path(output)
-    else:
-        out_path = infile.parent / "output"
-    click.echo(f"Downloading file from {out_url} to {out_path}")
-    file_path = dolby.download(out_url=out_url, out_path=out_path, job_type=job_type)
-    click.echo(f"File {infile} processed and saved to {file_path}")
+
+def get_api_key() -> str:
+    if "DOLBY_API_KEY" in os.environ:
+        # Key already set. Do nothing.
+        return os.environ["DOLBY_API_KEY"]
+
+    click.echo("This tool requires a valid API key for Dolby.io.")
+    return click.prompt("Enter your Dolby.io API key")
