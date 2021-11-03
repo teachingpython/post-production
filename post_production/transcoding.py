5604f5ebbf9385a785466f8c14521a9757e3f0ac
Sean Tibor <40579452+seantibor@users.noreply.github.com>
2021-11-02 22:39:58 -0400

    moved normalization for the entire file, not just individuals

--- post_production/transcoding.py
+++ post_production/transcoding.py
@@ -13,6 +13,11 @@ def transcode(
     output_file: Optional[Path] = typer.Option(
         None, help="Path to output file location"
     ),
+    normalize: Optional[bool] = typer.Option(False, help="Normalize the audio file"),
+    normalization_level: Optional[int] = typer.Option(
+        19,
+        help="Normalization (in LUFS). '16' will result in -16 LUFS. defaults to -19 LUFS",
+    ),
     intro_music: Optional[Path] = typer.Option(
         None, exists=True, help="Intro music to add before file."
     ),
@@ -20,19 +25,27 @@ def transcode(
         None, exists=True, help="Outro music to add after file."
     ),
 ):
+
     main_episode = ffmpeg.input(input_file)
     if intro_music:
         logger.info(f"Adding intro music from {intro_music}")
-        intro = ffmpeg.input(intro_music).filter("loudnorm", i=-23, dual_mono="true")
+        intro = ffmpeg.input(intro_music)
         main_episode = ffmpeg.filter(
             [intro, main_episode], "acrossfade", d=4, c1="tri", c2="nofade"
         )
     if outro_music:
         logger.info(f"Adding outro music from {outro_music}")
-        outro = ffmpeg.input(outro_music).filter("loudnorm", i=-23, dual_mono="true")
+        outro = ffmpeg.input(outro_music)
         main_episode = ffmpeg.filter(
             [main_episode, outro], "acrossfade", d=1, c1="nofade", c2="tri"
         )
+
+    if normalize:
+        n = -normalization_level
+        logger.info(f"Normalizing with level {n} LUFS.")
+        typer.echo(f"Normalizing with level {n} LUFS.")
+        main_episode = main_episode.filter("loudnorm", i=n, dual_mono="true")
+
     if output_file:
         output_path = Path(output_file)
     else:
